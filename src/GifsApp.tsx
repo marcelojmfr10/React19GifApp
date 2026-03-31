43e4b17e4407002abe912e278dcd9cc4e11381bd
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-03-31 09:25:09 -0600

    Custom hooks y useRef

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,31 +1,11 @@
-import { useState } from "react";
 import { GifList } from "./gifs/components/GifList";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
+import { useGifs } from "./gifs/hooks/useGifs";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
-import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
-import { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-  const [gifs, setGifs] = useState<Gif[]>([]);
-
-  const handleTermClicked = async (term: string) => {
-    const gifs = await getGifsByQuery(term);
-    setGifs(gifs);
-  };
-
-  const handleSearch = async (query: string = "") => {
-    query = query.trim().toLowerCase();
-    if (query.length === 0) return;
-
-    if (previousTerms.includes(query)) return;
-
-    setPreviousTerms([query, ...previousTerms].splice(0, 8));
-    const gifs = await getGifsByQuery(query);
-    setGifs(gifs);
-  };
-
+  const { previousTerms, gifs, handleSearch, handleTermClicked } = useGifs();
   return (
     <>
       <CustomHeader
